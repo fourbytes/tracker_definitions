e942a183b4346668ccd0a87a2a740a82b78990e5
Oscar Rainford <oscar@fourbytes.me>
2018-11-16 23:42:44 +1100

    rm -Rf for .git

--- update.sh
+++ update.sh
@@ -4,7 +4,7 @@ TEMP=$(mktemp -d)
 git clone --depth 1 git@github.com:Jackett/Jackett.git $TEMP
 
 cp $TEMP/src/Jackett.Common/Definitions/*.yml .
-rm -R $TEMP
+rm -Rf $TEMP
 
 # git add .
 # git commit -m "Updated definitions."
